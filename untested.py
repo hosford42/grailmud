7ea08167b8190afd9b2c6b52fcca2de4bf923934
free.condiments <free.condiments@9d540161-2e2a-0410-b27f-9f768c06348c>
2007-02-25 20:13:36 +0000

    more descriptive naming in untested.py

--- untested.py
+++ untested.py
@@ -1,11 +1,11 @@
 import os
 
-def main(directory):
+def discover_untested(directory):
     for rawfilename in os.listdir(directory):
         if rawfilename not in ('.svn', 'test'):
             filename = os.path.join(directory, rawfilename)
             if os.path.isdir(filename):
-                main(filename)
+                discover_untested(filename)
             elif filename.endswith('.py'):
                 if not os.access(os.path.join(directory, 'test', 
                                               'test_%s' % rawfilename), 
@@ -14,5 +14,5 @@ def main(directory):
                         print '%s not tested!' % filename
 
 if __name__ == '__main__':
-    main(os.path.join(os.curdir, 'grailmud'))
+    discover_untested(os.path.join(os.curdir, 'grailmud'))
 
