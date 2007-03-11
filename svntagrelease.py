1e1ce7743def20ecc10ab59d265a176bdbb7ace5
free.condiments <free.condiments@9d540161-2e2a-0410-b27f-9f768c06348c>
2007-03-11 23:10:04 +0000

    improvement to svntagrelease.py

--- svntagrelease.py
+++ svntagrelease.py
@@ -5,5 +5,5 @@ instring = open('grailmud/grailmud.egg-info/PKG-INFO').read()
 
 ver = re.search("^Version\:\s*(.*)$", instring, re.MULTILINE).group(1)
 
-subprocess.call("svn copy . https://grailmud.googlecode.com/svn/tags/"
-                "release-%s" % ver)
+subprocess.call('svn copy . https://grailmud.googlecode.com/svn/tags/'
+                'release-%(v)s -m "Tagging version %(v)s"' % {'v': ver})
